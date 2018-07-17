932094ae49292a1b6c239dd3baa41f10863f9717
Lautaro Woites <lautaro.woites@gmail.com>
2018-07-16 23:38:18 -0300

    rename pickle.js to picklify.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 /* eslint-env node, mocha */
 
-const picklejs = require('./pickle');
+const picklify = require('./picklify');
 const assert = require('chai').assert;
 
 describe('', function() {
     it('should serialize Dates', function() {
         const srcDate = new Date(2018, 1, 1);
 
-        let s = new picklejs.Serializer().serialize(srcDate);
-        let loader = new picklejs.Loader();
+        let s = new picklify.Serializer().serialize(srcDate);
+        let loader = new picklify.Loader();
         loader.initialize(s);
         let loaded = loader.buildObjects();
 
